fe20d06396865f53ffeafe5646479d1aadb04bc4
Oleksandr Kovalenko <motaem@gmail.com>
2017-05-09 18:02:02 +0300

    Lesson1: Clock
    http://catlikecoding.com/unity/tutorials/clock/

---
+++ learncatlikecoding/Assets/Clock/Scripts/ClockAnimator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System;
+
+public class ClockAnimator : MonoBehaviour
+{
+  public Transform Hours;
+  public Transform Minuutes;
+  public Transform Seconds;
+
+  private const float _hoursToDegrees = 360f / 12f;
+  private const float _minuntesToDegrees =  360f / 60f;
+  private const float _secondsToDegrees =  360f / 60f;
+
+  public bool AnalogClock;
+
+  private void Update()
+  {
+    if (AnalogClock)
+    {
+      TimeSpan timespan = DateTime.Now.TimeOfDay;
+      Hours.localRotation = Quaternion.Euler(0f, 0f, (float)timespan.TotalHours * -_hoursToDegrees);
+      Minuutes.localRotation = Quaternion.Euler(0f, 0f, (float)timespan.TotalMinutes * -_minuntesToDegrees);
+      Seconds.localRotation = Quaternion.Euler(0f, 0f, (float)timespan.Seconds * -_secondsToDegrees);
+    }
+    else
+    {
+      DateTime time = DateTime.Now;
+      Hours.localRotation = Quaternion.Euler(0f, 0f, time.Hour * -_hoursToDegrees);
+      Minuutes.localRotation = Quaternion.Euler(0f, 0f, time.Minute * -_minuntesToDegrees);
+      Seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * -_secondsToDegrees);
+    }
+  }
+}
